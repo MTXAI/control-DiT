432faf3f0759b46dc8060a9d90033128ed6c63d6
freekatzclear <katz@mtxai.io>
2024-05-26 20:27:28 +0800

    update

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # build source code
 rm -rf output/code
+rm -rf output/code.tar.gz
 mkdir -p output/code
 
 cp ./*.py output/code
@@ -12,5 +13,5 @@ cp -r ./models output/code
 cp -r ./train_options output/code
 cp -r ./utils output/code
 
-cd output/code
-tar -zcvf code.tar.gz ./*
+cd output/
+tar -zcvf code.tar.gz ./code/*
--- run.sh
+++ run.sh
@@ -3,9 +3,10 @@
 set -x
 input=$GEMINI_DATA_IN1/imagenette2/train/
 output=$GEMINI_DATA_OUT/imagenette_processed_train
-index=./annotations/imagenet_class_index.json
+code=$GEMINI_CODE/code
+index=$code/annotations/imagenet_class_index.json
 pretrain=$GEMINI_PRETRAIN/intel-isl_MiDaS_master
 
-python3 $GEMINI_RUN/preprocess.py --input $input --output $output \
+python3 $code/preprocess.py --input $input --output $output \
   --num-workers 4 --image-size 256 --class-index $index \
   --deep-model $pretrain --deep-model-source local --batch-size 4

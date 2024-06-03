1f65e0c079a6795169930651d9112cfd4d20f9b4
freekatz <katz@mtxai.io>
2024-06-04 02:46:07 +0800

    update run

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ index=$code/annotations/imagenet_class_index.json
 pretrain=$GEMINI_PRETRAIN/intel-isl_MiDaS_master
 pretrain2=$GEMINI_PRETRAIN2/sd-vae-ft-ema
 
-python3 $code/preprocess.py --input $input --output $output \
+time accelerate launch $code/preprocess.py --input $input --output $output \
   --num-workers 4 --image-size 256 --class-index $index \
   --deep-model $pretrain --deep-model-source local \
   --vae-model $pretrain2

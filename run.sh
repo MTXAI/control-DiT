4aadefda59fdc7da1d6e70d2f35073223dd207ee
freekatz <katz@mtxai.io>
2024-06-04 03:31:58 +0800

    update run

--- run.sh
+++ run.sh
@@ -14,10 +14,9 @@ input=$GEMINI_DATA_IN1/imagenette_processed_train
 output=$GEMINI_DATA_OUT/control-dit_train_baseline-v4
 model_type=DiT-XL/2
 dit_model=$GEMINI_PRETRAIN/checkpoints/${model_name}
-vae_model=$GEMINI_PRETRAIN2/sd-vae-ft-ema
 
 
 time accelerate launch $code/train.py --input $input --output $output \
   --model-type $model_type --dit-model-path $dit_model \
-  --vae-model-path $vae_model --image-size $image_size \
-  --epochs $epochs --batch-size $batch_size --num-workers $num_workers
+  --image-size $image_size --epochs $epochs --batch-size $batch_size \
+  --num-workers $num_workers

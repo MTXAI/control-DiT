782f2810362aeeafc0d573f178839699d30bef49
freekatz <katz@mtxai.io>
2024-06-03 20:13:54 +0800

    update build

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ set -x
 
 code=$GEMINI_CODE/code
 
-input=$GEMINI_DATA_OUT/imagenette_processed_train
+input=$GEMINI_DATA_IN1/imagenette_processed_train
 output=$GEMINI_DATA_OUT/control-dit_train_baseline-v4
 model_type=DiT-XL/2
-dit_model=$GEMINI_PRETRAIN2/checkpoints/DiT-XL-2-256x256.pt
+dit_model=$GEMINI_PRETRAIN/checkpoints/DiT-XL-2-256x256.pt
 
 image_size=256
 

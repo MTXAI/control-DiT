a45aa6b4146fada4608eb8b38630214b4d30f5fa
freekatz <katz@mtxai.io>
2024-06-14 02:23:10 +0800

    update sample

--- temp.py
+++ temp.py
@@ -5,10 +5,10 @@ from src.utils.model import load_depth_model
 
 midas, transform = load_depth_model(model_type="DPT_Large", device='cpu')
 
-x = cv2.imread("./image-171.png")
+x = cv2.imread("hack/chores/5-train-baseline-2-opt/sample-64-4090-4-epoch900_result.png")
 
 z = cv2_to_depth(x, midas, transform, 'cpu')
 z = z.detach().cpu().numpy()
-cv2.imwrite('./image-171-depth.png', z)
+cv2.imwrite('sample-64-4090-4-epoch900_result-depth.png', z)
 
 
